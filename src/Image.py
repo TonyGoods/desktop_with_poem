52b355ebbaa3f44e83df5db32fbc4a10c9f0ec0b
ZGuo32 <ZGuo32@its.jnj.com>
2019-03-06 23:01:45 +0800

    add poem to image, and set the image to be the desktop

---
+++ src/Image.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/6 14:34
+# @Author  : Tony Goods
+# @FileName: Image.py
+# @Software: PyCharm
---
+++ src/utils.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/6 14:46
+# @Author  : Tony Goods
+# @FileName: utils.py
+# @Software: PyCharm
